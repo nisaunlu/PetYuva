d86b5c51e18a7f5042129a2d23312d216912557a
Nuran Guler <gulernuran9@gmail.com>
2025-02-27 14:18:34 +0300

    dosya-eklendi

--- App.tsx
+++ App.tsx
@@ -1,22 +1,14 @@
 
 import React from "react";
-import LoginScreen from './src/screens/LoginScreen.tsx';
-import { View } from "react-native";
+import RegisterScreen from "./src/screens/RegisterScreen";
 
 
 
 function App(): React.JSX.Element {
 
   return(
-    <View style={{
-      flex:1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor:rgb(14, 1, 1)
+ <RegisterScreen/>
 
-    }}>
-
-    </View>
   )
 }
 
