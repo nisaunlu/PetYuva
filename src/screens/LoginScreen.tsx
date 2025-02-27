764b3ebaf8127f573878b9c420e397000ebb1273
nisaunlu <nisaunlv11@gmail.com>
2025-02-27 13:48:33 +0300

    Initial commit for PetYuva project

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LoginScreen = () => {
+    return(
+        <View style = {{justifyContent:'center', flex:1, alignItems:'center'}}> 
+            <Text>Merhava</Text>
+        </View>
+
+    )
+}
+export default LoginScreen;
